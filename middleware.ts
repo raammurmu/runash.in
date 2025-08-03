09e2f6a8a33111240fe2cd7f5ff35cdc5092ef90
Ram Murmu <runashInc@outlook.com>
2025-08-03 20:55:09 +0530

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -49,7 +49,7 @@ export async function middleware(request: NextRequest) {
     "/ai-overview",
     "/models",
     "/checkout",
-    "/seller/seller-dashboard",
+    "/seller-dashboard",
     "/live",
     "/mobile",
     
